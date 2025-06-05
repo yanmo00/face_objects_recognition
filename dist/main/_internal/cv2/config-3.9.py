6da6982eda89d252a63d6235b1b07131d958c9d3
言默 <129597196+yanmo00@users.noreply.github.com>
2025-06-05 11:31:30 +0800

    添加.gitignore忽略构建产物
    Former-commit-id: 8221363603761edc7f3e60fe67fcb9bcc89a6ec0

--- dist/main/_internal/cv2/config-3.9.py
+++
@@ -1,3 +0,0 @@
-PYTHON_EXTENSIONS_PATHS = [
-    os.path.join('D:/software/Anaconda/envs/ultralytics/Lib/site-packages/cv2', 'python-3.9')
-] + PYTHON_EXTENSIONS_PATHS
--- dist/main/_internal/cv2/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BINARIES_PATHS = [
-    os.path.join('D:/software/Anaconda/envs/ultralytics/Library', 'bin')
-] + BINARIES_PATHS
